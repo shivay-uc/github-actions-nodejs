58fda492eba75ee77ddd09b2f1dc084a1b5cf4b3
shivay-uc <98797694+shivay-uc@users.noreply.github.com>
2022-07-15 17:03:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ require('dotenv').config();
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log('Example app listening on port 3000!'));
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
